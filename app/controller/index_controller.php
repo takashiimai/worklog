465d5d4eb9b02ec7e0a2ddfba867dfc18040b2aa
imai <takashi.imai@onionnews.co.jp>
2017-02-27 19:58:10 +0900

    削除

--- app/controller/index_controller.php
+++ app/controller/index_controller.php
@@ -6,6 +6,10 @@ class index_controller extends app_controller {
         $this->model('sub/member_model');
         $result = $this->member_model->get_all();
         var_dump($result);
+
+        $this->model('sub/account_model');
+        $result = $this->account_model->get_all();
+        var_dump($result);
     }
 }
 
